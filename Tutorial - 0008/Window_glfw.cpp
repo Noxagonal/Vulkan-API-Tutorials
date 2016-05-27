c66ad8b230e012040131f0c54f7fe0401fd62ba0
teaganc <t.chouinard@hotmail.com>
2016-05-26 21:39:38 -0600

    Refactor

--- Tutorial - 0008/Window_glfw.cpp
+++ Tutorial - 0008/Window_glfw.cpp
@@ -12,20 +12,9 @@
 
 void Window::_InitOSWindow()
 {	
-	if ( glfwInit() == GLFW_FALSE ) {
-		assert ( 0 && "GLFW failed to initialize." );
-		return;
-	}
-	if ( glfwVulkanSupported() == GLFW_FALSE ) {
-		glfwTerminate();
-		assert( 0 && "GLFW reported Vulkan is not supported." );
-		return;
-	}
-
 	glfwWindowHint( GLFW_CLIENT_API, GLFW_NO_API );
 	_glfw_window = glfwCreateWindow( _surface_size_x, _surface_size_y, _window_name.c_str(), nullptr, nullptr );
 	glfwGetFramebufferSize ( _glfw_window, (int*)&_surface_size_x, (int*)&_surface_size_y );
-
 }
 
 void Window::_DeInitOSWindow()
